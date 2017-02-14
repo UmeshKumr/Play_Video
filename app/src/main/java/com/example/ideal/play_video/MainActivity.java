48a6c99681342bbb357c0562d49f6ba9759a0564
UmeshKumr <umeshtaneja.123@gmail.com>
2017-02-14 16:12:41 +1400

    change

--- app/src/main/java/com/example/ideal/play_video/MainActivity.java
+++ app/src/main/java/com/example/ideal/play_video/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//one
 
             getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>"+title+"</font>"));
             // getSupportActionBar().setTitle("title");
